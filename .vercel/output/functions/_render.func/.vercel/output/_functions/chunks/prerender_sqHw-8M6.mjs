f1370bf4c896923c41d3f8c3d2159b72aad6a5c6
Andrzej Marek <andrzejmarek@Andrzejs-Mac-Studio.local>
2024-05-26 11:46:21 +0200

    vercel and frame src

---
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/prerender_sqHw-8M6.mjs
@@ -0,0 +1,8 @@
+const noop = () => {};
+export const _ = noop;
+export const a = noop;
+export const c = noop;
+export const f = noop;
+export const i = noop;
+export const m = noop;
+export const r = noop;
