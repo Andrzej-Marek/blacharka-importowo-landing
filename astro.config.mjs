cb5876fedddaf8c083f8414e046c0f1211138c2a
Andrzej Marek <andrzejmarek@Andrzejs-Mac-Studio.local>
2024-05-26 11:32:57 +0200

    stro as hybrid

--- astro.config.mjs
+++ astro.config.mjs
@@ -131,7 +131,7 @@ export default defineConfig({
     gzip: false,
     brotli: true
   })],
-  output: "static",
+  output: "hybrid",
   experimental: {
     clientPrerender: true,
     directRenderScript: true
