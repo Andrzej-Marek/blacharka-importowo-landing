0ffe6066fcb78a450ee44087a94cad1bde654b6d
Andrzej Marek <andrzejmarek@MacBook-Pro.local>
2024-09-25 09:17:03 +0200

    build

--- .vercel/output/functions/_render.func/.vercel/output/_functions/entry.mjs
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/entry.mjs
@@ -1,26 +1,28 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports } from './chunks/entrypoint_CgxU8Nez.mjs';
-import { manifest } from './manifest_DEZmjrxX.mjs';
+import { manifest } from './manifest_BLoq_yPR.mjs';
 import { onRequest } from './_noop-middleware.mjs';
 
 const _page0 = () => import('./pages/_image.astro.mjs');
 const _page1 = () => import('./pages/404.astro.mjs');
 const _page2 = () => import('./pages/contact.astro.mjs');
 const _page3 = () => import('./pages/favicon.ico.astro.mjs');
-const _page4 = () => import('./pages/manifest.json.astro.mjs');
-const _page5 = () => import('./pages/robots.txt.astro.mjs');
-const _page6 = () => import('./pages/index.astro.mjs');
-const _page7 = () => import('./pages/_---slug_.astro.mjs');
+const _page4 = () => import('./pages/gallery.astro.mjs');
+const _page5 = () => import('./pages/manifest.json.astro.mjs');
+const _page6 = () => import('./pages/robots.txt.astro.mjs');
+const _page7 = () => import('./pages/index.astro.mjs');
+const _page8 = () => import('./pages/_---slug_.astro.mjs');
 
 const pageMap = new Map([
     ["node_modules/astro/dist/assets/endpoint/generic.js", _page0],
     ["src/pages/404.astro", _page1],
     ["src/pages/contact.astro", _page2],
     ["src/pages/favicon.ico.ts", _page3],
-    ["src/pages/manifest.json.ts", _page4],
-    ["src/pages/robots.txt.ts", _page5],
-    ["src/pages/index.astro", _page6],
-    ["node_modules/@astrojs/starlight/index.astro", _page7]
+    ["src/pages/gallery.astro", _page4],
+    ["src/pages/manifest.json.ts", _page5],
+    ["src/pages/robots.txt.ts", _page6],
+    ["src/pages/index.astro", _page7],
+    ["node_modules/@astrojs/starlight/index.astro", _page8]
 ]);
 const serverIslandMap = new Map();
 
@@ -31,7 +33,7 @@ const _manifest = Object.assign(manifest, {
     middleware: onRequest
 });
 const _args = {
-    "middlewareSecret": "6ee1fade-0d58-4e2c-ab15-c0cf1400a41f",
+    "middlewareSecret": "38a90283-04d9-4d28-ae08-3499718e30ff",
     "skewProtection": false
 };
 const _exports = createExports(_manifest, _args);
