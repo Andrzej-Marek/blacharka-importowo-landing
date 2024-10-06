7bc1a0064d427282b0ad29da89f9cb167ed4dfad
Andrzej Marek <andrzejmarek116@gmail.com>
2024-10-06 08:57:27 +0200

    add vercel custom event

--- src/lib/gtm.ts
+++ src/lib/gtm.ts
@@ -1,14 +1,16 @@
-"use client";
+import { track } from "@vercel/analytics";
 import { sendGTMEvent } from "@next/third-parties/google";
 
 export const gtmPageView = (rest = {}) => {
   if (typeof window === "undefined") return;
 
   sendGTMEvent({ event: "page_view", url: window.location.href, ...rest });
+  track("page_view", { url: window.location.href, ...rest });
 };
 
 export const gtmOnPhoneClick = (rest = {}) => {
   if (typeof window === "undefined") return;
 
   sendGTMEvent({ event: "phone_clicked", url: window.location.href, ...rest });
+  track("phone_clicked", { url: window.location.href, ...rest });
 };
