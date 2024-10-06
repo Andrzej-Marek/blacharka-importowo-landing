914dd881518f5360d71a4f6ae91ede7205c6439e
Andrzej Marek <andrzejmarek116@gmail.com>
2024-10-06 08:51:49 +0200

    no script google tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -55,6 +55,14 @@ export default function RootLayout({
     <html lang="pl">
       <GoogleTagManager gtmId="GTM-NVSM33GZ" />
       <body className={`${poppins.className}`}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NVSM33GZ"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <Navbar />
         {children}
         <Analytics />
